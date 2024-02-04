929e5eaa98b543559f068693dd71b48e1f5c686e
GitYaw <118967566+GitYaw@users.noreply.github.com>
2024-02-04 08:33:38 +0000

    Added sample data and API endpoint

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Sacramento, CA',
+    'salary': '$100,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Raleigh, NC',
+    'salary': 'Rs. $110,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote, USA'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Tampa Bay, FL',
+    'salary': '$120,000'
+  }
+]
+
 @app.route("/")
 def hello_jobs():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company='Jobs & Co. Ltd.')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
